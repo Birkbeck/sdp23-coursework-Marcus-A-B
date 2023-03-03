3fcc1d933877de07b89317d34852511a2b5c961b
Marcus-A-B <mburns05@student.bbk.ac.uk>
2023-03-03 13:41:54 +0000

    Added JavaDoc for MulInstruction.java

--- src/sml/instruction/MulInstruction.java
+++ src/sml/instruction/MulInstruction.java
@@ -6,10 +6,11 @@ import sml.RegisterName;
 
 import java.util.Objects;
 
-// TODO: write a JavaDoc for the class
 
 /**
- * @author
+ * Represents a Mul Instruction which multiplies the contents of the source register and the result register
+ * and stores the result in the first register.
+ * @author Marcus Burns
  */
 
 public class MulInstruction extends Instruction {
@@ -18,12 +19,27 @@ public class MulInstruction extends Instruction {
 
 	public static final String OP_CODE = "mul";
 
+	/**
+	 * Constructor: n Mul instruction with a label, a result register,
+	 * and a source register. (The registers are enumerated).
+	 *
+	 * @param label optional label (can be null)
+	 * @param result a register where the result of the mul operation is stored
+	 * @param source a register which contains an integer that multiplied with
+	 * the integer contained in the result register
+	 */
 	public MulInstruction(String label, RegisterName result, RegisterName source) {
 		super(label, OP_CODE);
 		this.result = result;
 		this.source = source;
 	}
 
+	/**
+	 * Executes the Mul instruction in the given machine
+	 *
+	 * @param m the machine the instruction runs on
+	 * @return the address of the next instruction to run
+	 */
 	@Override
 	public int execute(Machine m) {
 		int value1 = m.getRegisters().get(result);
@@ -32,11 +48,23 @@ public class MulInstruction extends Instruction {
 		return NORMAL_PROGRAM_COUNTER_UPDATE;
 	}
 
+	/**
+	 * A string representation of the Mul instruction
+	 *
+	 * @return returns the OPP_CODE and result and source registers in a formatted string
+	 */
 	@Override
 	public String toString() {
 		return getLabelString() + getOpcode() + " " + result + " " + source;
 	}
 
+	/**
+	 * Compares this object to the specified object for equality.
+	 *
+	 * @param o the object to compare this instance to.
+	 * @return true if the specified object is an instance of SubInstruction and
+	 * the result and source registers are equal.
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (o instanceof MulInstruction) {
@@ -46,6 +74,11 @@ public class MulInstruction extends Instruction {
 		return false;
 	}
 
+	/**
+	 * Returns a hash code value for this object based on the result register,
+	 * the source register, and the OP_CODE.
+	 * @return a hash code value for this object
+	 */
 	@Override
 	public int hashCode() {return Objects.hash(result, source, OP_CODE);}
 }
